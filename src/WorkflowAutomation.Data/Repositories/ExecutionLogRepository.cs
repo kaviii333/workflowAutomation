ac52ec10ca17e8807d9416d090124aff25c0d13e
kaviii333 <muraly003@gmail.com>
2026-03-04 13:22:43 +0530

    Repositories Check-in

---
+++ src/WorkflowAutomation.Data/Repositories/ExecutionLogRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowAutomation.Data.Models;
+
+namespace WorkflowAutomation.Data.Repositories
+{
+    public class ExecutionLogRepository
+    {
+        private readonly WorkflowDbContext _context;
+
+        public ExecutionLogRepository(WorkflowDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ExecutionLog>> GetLogsByExecutionIdAsync(Guid executionId)
+        {
+            return await _context.ExecutionLogs
+                .Where(l => l.ExecutionId == executionId)
+                .ToListAsync();
+        }
+    }
+}
