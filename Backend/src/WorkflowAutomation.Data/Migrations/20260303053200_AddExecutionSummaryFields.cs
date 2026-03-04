c57e92f00a99b17a324a97f6e253f96b81c489fc
kaviii333 <muraly003@gmail.com>
2026-03-04 15:24:57 +0530

    Add files via upload

---
+++ Backend/src/WorkflowAutomation.Data/Migrations/20260303053200_AddExecutionSummaryFields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkflowAutomation.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExecutionSummaryFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalDurationSeconds",
+                table: "Executions",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalDurationSeconds",
+                table: "Executions");
+        }
+    }
+}
