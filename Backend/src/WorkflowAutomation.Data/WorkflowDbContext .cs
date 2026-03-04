9cffd7588aaaf220a1a16acd0da551c36ee9fc37
kaviii333 <muraly003@gmail.com>
2026-03-04 14:57:19 +0530

    DB Context check-in

---
+++ Backend/src/WorkflowAutomation.Data/WorkflowDbContext .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using WorkflowAutomation.Data.Models;
+
+namespace WorkflowAutomation.Data
+{
+        public class WorkflowDbContext : DbContext
+        {
+            public WorkflowDbContext(DbContextOptions<WorkflowDbContext> options) : base(options) { }
+
+            public DbSet<Workflow> Workflows { get; set; }
+            public DbSet<Step> Steps { get; set; }
+            public DbSet<RuleList> Rules { get; set; }
+            public DbSet<Execution> Executions { get; set; }
+            public DbSet<ExecutionLog> ExecutionLogs { get; set; }
+        }
+
+}
---
+++ Backend/src/WorkflowAutomation.Data/WorkflowDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkflowAutomation.Data
+{
+    public class WorkflowDbContextFactory : IDesignTimeDbContextFactory<WorkflowDbContext>
+    {
+        public WorkflowDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WorkflowDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=local;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+            return new WorkflowDbContext(optionsBuilder.Options);
+        }
+    }
+
+}
