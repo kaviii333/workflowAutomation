e6bd309ff5f02766d7c7965c2b82a66d2086ea39
kaviii333 <muraly003@gmail.com>
2026-03-04 13:20:33 +0530

    Models Check-in
    Models Check-in

---
+++ src/WorkflowAutomation.Data/Models/Workflow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkflowAutomation.Data.Models
+{
+    public class Workflow
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Version { get; set; }
+        public bool IsActive { get; set; }
+        public string InputSchema { get; set; } = string.Empty;
+        public Guid StartStepId { get; set; }
+        public ICollection<Step> Steps { get; set; } = new List<Step>();
+    }
+}
