cf68c862b2e3d0e553fd70d70a7dcb9c91137c87
kaviii333 <muraly003@gmail.com>
2026-03-04 14:47:11 +0530

    DTOs Check-in

---
+++ Backend/src/WorkflowAutomation.Api/DTOs/WorkflowDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkflowAutomation.Api.DTOs
+{
+    public class WorkflowDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string InputSchema { get; set; } = string.Empty;
+        public Guid StartStepId { get; set; }
+    }
+}
