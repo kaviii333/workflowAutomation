521e3a895cc4e258118a8c10427589b915907065
kaviii333 <muraly003@gmail.com>
2026-03-04 18:31:00 +0530

    Update seed.sql

--- Scripts/seed.sql
+++ Scripts/seed.sql
@@ -1,3 +1,87 @@
+
+CREATE TABLE [dbo].[ExecutionLogs] (
+    [Id]               UNIQUEIDENTIFIER NOT NULL,
+    [ExecutionId]      UNIQUEIDENTIFIER NOT NULL,
+    [StepName]         NVARCHAR (MAX)   NOT NULL,
+    [StepType]         NVARCHAR (MAX)   NOT NULL,
+    [EvaluatedRules]   NVARCHAR (MAX)   NOT NULL,
+    [SelectedNextStep] NVARCHAR (MAX)   NOT NULL,
+    [Status]           NVARCHAR (MAX)   NOT NULL,
+    [ErrorMessage]     NVARCHAR (MAX)   NOT NULL,
+    [StartedAt]        DATETIME2 (7)    NOT NULL,
+    [EndedAt]          DATETIME2 (7)    NULL,
+    CONSTRAINT [PK_ExecutionLogs] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ExecutionLogs_Executions_ExecutionId] FOREIGN KEY ([ExecutionId]) REFERENCES [dbo].[Executions] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ExecutionLogs_ExecutionId]
+    ON [dbo].[ExecutionLogs]([ExecutionId] ASC);
+
+CREATE TABLE [dbo].[Executions] (
+    [Id]                   UNIQUEIDENTIFIER NOT NULL,
+    [WorkflowId]           UNIQUEIDENTIFIER NOT NULL,
+    [WorkflowVersion]      INT              NOT NULL,
+    [Status]               NVARCHAR (MAX)   NOT NULL,
+    [Data]                 NVARCHAR (MAX)   NOT NULL,
+    [CurrentStepId]        UNIQUEIDENTIFIER NOT NULL,
+    [StartedAt]            DATETIME2 (7)    NOT NULL,
+    [EndedAt]              DATETIME2 (7)    NULL,
+    [TotalDurationSeconds] FLOAT (53)       NULL,
+    CONSTRAINT [PK_Executions] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Executions_Workflows_WorkflowId] FOREIGN KEY ([WorkflowId]) REFERENCES [dbo].[Workflows] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Executions_WorkflowId]
+    ON [dbo].[Executions]([WorkflowId] ASC);
+
+CREATE TABLE [dbo].[Rules] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [StepId]     UNIQUEIDENTIFIER NOT NULL,
+    [Condition]  NVARCHAR (MAX)   NOT NULL,
+    [NextStepId] UNIQUEIDENTIFIER NOT NULL,
+    [Priority]   INT              NOT NULL,
+    CONSTRAINT [PK_Rules] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Rules_Steps_StepId] FOREIGN KEY ([StepId]) REFERENCES [dbo].[Steps] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Rules_StepId]
+    ON [dbo].[Rules]([StepId] ASC);
+
+CREATE TABLE [dbo].[Steps] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [WorkflowId] UNIQUEIDENTIFIER NOT NULL,
+    [Name]       NVARCHAR (MAX)   NOT NULL,
+    [StepType]   NVARCHAR (MAX)   NOT NULL,
+    [Order]      INT              NOT NULL,
+    [Metadata]   NVARCHAR (MAX)   NOT NULL,
+    CONSTRAINT [PK_Steps] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Steps_Workflows_WorkflowId] FOREIGN KEY ([WorkflowId]) REFERENCES [dbo].[Workflows] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Steps_WorkflowId]
+    ON [dbo].[Steps]([WorkflowId] ASC);
+
+
+CREATE TABLE [dbo].[Workflows] (
+    [Id]          UNIQUEIDENTIFIER NOT NULL,
+    [Name]        NVARCHAR (MAX)   NOT NULL,
+    [Version]     INT              NOT NULL,
+    [IsActive]    BIT              NOT NULL,
+    [InputSchema] NVARCHAR (MAX)   NOT NULL,
+    [StartStepId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Workflows] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
 -- ================================
 -- Workflows
 -- ================================
